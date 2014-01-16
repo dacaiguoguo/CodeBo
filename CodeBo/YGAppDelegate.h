8c07ec88d526987905a8d3537c52a85125fb3560
dacaiguoguo <dacaiguoguo@163.com>
2014-01-16 21:54:34 +0800

    Initial Commit

---
+++ CodeBo/YGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YGAppDelegate.h
+//  CodeBo
+//
+//  Created by sunyanguo on 14-1-16.
+//  Copyright (c) 2014年 sunyanguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CodeBo/YGViewController.h
@@ -0,0 +1,13 @@
+//
+//  YGViewController.h
+//  CodeBo
+//
+//  Created by sunyanguo on 14-1-16.
+//  Copyright (c) 2014年 sunyanguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YGViewController : UIViewController
+
+@end
